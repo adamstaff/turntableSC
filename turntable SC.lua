dc745c5a6a198ced5dc403a61f5eecfad7bc3e6f
Adam Staff <adamjstaff@gmail.com>
2025-02-10 18:28:09 +0000

    mac mini adjustments

--- turntable SC.lua
+++ turntable SC.lua
@@ -6,21 +6,37 @@ Engine_turntable : CroneEngine {
 
 		// add SynthDefs
 		SynthDef("turntable", {
-			arg out=0, rate, doloop;
-			var playback;
-			var lag = Lag3.kr(rate, 1.1);
-			playback = PlayBuf.ar(2, 0, lag, loop: doloop);
+			arg bufNum = 0, out=0,
+			t_trigger, t_poll,
+			rate = 1, doloop = 1.0,	stiffness = 1.1, goto = 0;
+			var playback, playhead, position, position_deci;
+			playhead = Phasor.ar(
+				trig: t_trigger,
+				rate: rate,
+				start: 0,
+				end: BufFrames.kr(b.bufnum),
+				resetPos: goto
+			);
+			position = Lag3.ar(playhead, stiffness);
+			position_deci = position / BufFrames.kr(b.bufnum);
+			playback = BufRd.ar(
+				numChannels: 2,
+				bufnum: 0,
+				phase: position,
+				loop: doloop;
+			);
+			Poll.kr(t_poll, position_deci, "position");
+			Out.ar(out, playback);
+		}).play;
 
-		Out.ar(out, playback);
-		}	).play;
-
-  Server.default.sync;
+	Server.default.sync;
 		
 	// let's create an Dictionary (an unordered associative collection)
-	//   to store parameter values, initialized to defaults.
+	//   to store parameter values, initialized to defaults
+	// for user control
 	params = Dictionary.newFrom([
-		\turntBuff,
-		\rate, 1,
+		\turntBuf, {},
+		\rate, 0.0,
 		\doloop, 1
 		;
 	]);
@@ -36,22 +52,35 @@ Engine_turntable : CroneEngine {
 			params[key] = msg[1];
 		});
 	});
-
+	
   // command to load file into buffer
   // "loadfile" will be name of Lua file parameter
   // i.e. engine.loadfile(filename,number_of_samples)
-	this.addCommand("loadfile","isi", { arg msg;
-    // empty buffer
-    turntBuff.free;
-    // post a friendly message
-    postln("loading "++msg[2]++" samples of "++msg[1]);
-    // write to the buffer
-    turntBuff = Buffer.read(context.server,msg[1],numFrames:msg[2]);
-    // set correct buffer number & stop turntable
-    turntable.set(
-        \bufnum,0,
-        \rate, 0
-    );;
+	this.addCommand("loadfile","si", { arg msg;
+	    // empty buffer
+	   turntBuf.free;
+	    // post a friendly message
+	    postln("loading "++msg[2]++" samples of "++msg[1]);
+	    // write to the buffer
+    	turntBuff = Buffer.read(context.server,msg[1],numFrames:msg[2]);
+	    // set correct buffer number & stop turntable
+	    turntable.set(
+	        \rate, 0.0, \goto, 0.0, \t_trigger, 1
+	    );;
+	};
+	
+	//command to poll the position
+	this.addCommand("pos_poll","f", { arg msg;
+	    turntable.set(
+	    	\t_poll,1
+	    )
+	};
+
+	//command to poll the position - decimal position
+	this.addCommand("goto","f", { arg msg;
+	    turntable.set(
+	    	\goto,msg
+	    )
 	};
 
 }
